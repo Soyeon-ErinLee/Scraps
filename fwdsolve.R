55e73d16cb14f79bc05158246e84dc6342051335
Erin <65602256+Soyeon-ErinLee@users.noreply.github.com>
2020-06-09 00:54:42 +0900

    Create fwdsolve.R

---
+++ fwdsolve.R
@@ -0,0 +1,11 @@
+# Comparing time elapsed for solve() and fwdsolve/bwdsolve
+# Using solve() in R is not efficient!
+
+A=matrix(1:1000000, c(1000,1000))
+A[upper.tri(A)]=0
+B=matrix(5:1000004, c(1000,1000))
+x=solve(A)%*%B
+y=forwardsolve(A,B)
+is.logical(x==y)
+system.time(x)
+system.time(y)
